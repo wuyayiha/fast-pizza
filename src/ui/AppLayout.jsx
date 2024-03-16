70295545be781e3e58db3e3dc509008acf89c2e5
wuyayiha <m8j6ka@163.com>
2024-03-16 10:52:14 +0800

    first commit

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,24 @@
+import CartOverview from "../features/cart/CartOverview";
+import Header from "./Header";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
+
+function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
+  return (
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {isLoading && <Loader />}
+      <Header />
+
+      <main className="mx-auto max-w-3xl">
+        <Outlet />
+      </main>
+
+      <CartOverview />
+    </div>
+  );
+}
+
+export default AppLayout;
